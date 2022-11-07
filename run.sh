e9821cd8bae26478073ee9293581cf4ec2e869b4
jlazear <jlazear@gmail.com>
2022-11-07 09:20:52 -0800

    Typo

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Starting controller...
-python ./lab_interface/controller.py & > controller.log
+python ./lab_interface/controller.py > controller.log & 
 sleep 2
 echo Starting user terminal...
 python ./user_terminal/user_terminal.py
